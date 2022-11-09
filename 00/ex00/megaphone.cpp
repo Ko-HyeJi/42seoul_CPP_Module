a6f79d3c22f95c8a6c946affe86847d08ab55a3a
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-11-09 18:50:03 +0900

    00_ex00

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 17:48:07 by hyko              #+#    #+#             */
+/*   Updated: 2022/11/09 18:48:12 by hyko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	}
+	else {
+		for (int i = 1; argv[i]; i++){
+			for (int j = 0; argv[i][j]; j++){
+				if (std::islower(argv[i][j]))
+					argv[i][j] = std::toupper(argv[i][j]);
+			}
+		}
+		for (int i = 1; argv[i]; i++){
+			std::cout << argv[i];
+		}
+		std::cout << "\n";
+	}
+	return (0);
+}
