ce7e6a23924dd0eb4fbb4307ff4a2d7f3a119a83
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-11-12 02:30:16 +0900

    [fix...] megaphone

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,32 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:48:07 by hyko              #+#    #+#             */
-/*   Updated: 2022/11/09 20:43:54 by hyko             ###   ########.fr       */
+/*   Updated: 2022/11/11 20:31:56 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int main(int argc, char **argv)
-{
+ getUppserString(const string& str) {
+
+}
+
+int main(int argc, char** argv) {
+	int cnt;
+	std::cin >> cnt;
+
 	if (argc == 1){
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	}
-	else {
-		for (int i = 1; argv[i]; i++){
-			for (int j = 0; argv[i][j]; j++){
-				if (std::islower(argv[i][j]))
-					argv[i][j] = std::toupper(argv[i][j]);
-			}
-		}
-		for (int i = 1; argv[i]; i++){
-			std::cout << argv[i];
-		}
-		std::cout << "\n";
+
+	std::string str;
+	for (int i = 1; i < cnt; i++) {
+		str = argv[i];
+		std::cout << getUpperString(str) << std::endl;
 	}
-	return (0);
 }
+
+
+
+
+// int main(int argc, char **argv)
+// {
+// 	if (argc == 1){
+// 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+// 	}
+// 	else {
+// 		for (int i = 1; argv[i]; i++){
+// 			for (int j = 0; argv[i][j]; j++){
+// 				if (std::islower(argv[i][j]))
+// 					argv[i][j] = std::toupper(argv[i][j]);
+// 			}
+// 		}
+// 		for (int i = 1; argv[i]; i++){
+// 			std::cout << argv[i];
+// 		}
+// 		std::cout << "\n";
+// 	}
+// 	return (0);
+// }
