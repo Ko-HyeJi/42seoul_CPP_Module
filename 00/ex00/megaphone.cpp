1369b3d2a5e47c1a719a24bc6933cc8ece91bfbd
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-11-14 10:38:22 +0900

    ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -13,44 +13,26 @@
 #include <iostream>
 #include <string>
 
- getUppserString(const string& str) {
-
+std::string getUpperString(std::string str)
+{
+	for (int i = 0; str[i]; i++){
+		if (std::islower(str[i]))
+			str[i] = std::toupper(str[i]);
+	}
+	return (str);
 }
 
-int main(int argc, char** argv) {
-	int cnt;
-	std::cin >> cnt;
-
+int main(int argc, char **argv)
+{
 	if (argc == 1){
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
 	}
-
 	std::string str;
-	for (int i = 1; i < cnt; i++) {
+	for (int i = 1; i < argc; i++){
 		str = argv[i];
-		std::cout << getUpperString(str) << std::endl;
+		std::cout << getUpperString(str);
 	}
+	std::cout << std::endl;
+	return (0);
 }
-
-
-
-
-// int main(int argc, char **argv)
-// {
-// 	if (argc == 1){
-// 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-// 	}
-// 	else {
-// 		for (int i = 1; argv[i]; i++){
-// 			for (int j = 0; argv[i][j]; j++){
-// 				if (std::islower(argv[i][j]))
-// 					argv[i][j] = std::toupper(argv[i][j]);
-// 			}
-// 		}
-// 		for (int i = 1; argv[i]; i++){
-// 			std::cout << argv[i];
-// 		}
-// 		std::cout << "\n";
-// 	}
-// 	return (0);
-// }
