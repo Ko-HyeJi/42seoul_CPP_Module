ce3732d4e2b8932308b3d382c5eee0310f8a3b09
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-11-16 16:17:35 +0900

    ex02

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -15,8 +15,8 @@
 int main(void)
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string *strPTR = &str;
-    std::string &strREF = str;
+    std::string* strPTR = &str;
+    std::string& strREF = str;
 
     std::cout << &str << std::endl;
     std::cout << strPTR << std::endl;
