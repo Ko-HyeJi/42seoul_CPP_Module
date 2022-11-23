1fd3b32e11385160b85b68e0bfcd04991b9942ae
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-11-23 17:51:26 +0900

    ex00

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -6,32 +6,28 @@
 /*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 23:43:57 by hyko              #+#    #+#             */
-/*   Updated: 2022/11/20 04:16:03 by hyko             ###   ########.fr       */
+/*   Updated: 2022/11/22 14:43:40 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-//기본 생성자
 Fixed::Fixed(void): _value(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-//소멸자
 Fixed::~Fixed(void)
 {
 	std::cout << "Destructor called" << std::endl;
 }
 
-//복사 생성자
 Fixed::Fixed(const Fixed& obj)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = obj;
 }
 
-//복사 대입 연산자 오버로딩
 Fixed& Fixed::operator=(const Fixed& obj)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 23:43:46 by hyko              #+#    #+#             */
-/*   Updated: 2022/11/20 04:16:25 by hyko             ###   ########.fr       */
+/*   Updated: 2022/11/22 14:43:25 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,13 @@ private:
 	static const int _bits = 8;	//소수의 비트 수
 
 public:
-	//기본 생성자 -> 고정소수점 값을 0으로 초기화
-	Fixed(void);
-	//소멸자
+	Fixed(void); //기본 생성자 -> 고정소수점 값을 0으로 초기화
 	~Fixed(void);
-	//복사 생성자
 	Fixed(const Fixed& obj);
-	//(복사)대입 연산자 오버로딩
 	Fixed& operator=(const Fixed& obj);
 
-	//고정소수점 값 원본을 반환
-	int getRawBits(void) const;
-	//고정소수점 값 원본을 설정
-	void setRawBits(int const raw);
+	int getRawBits(void) const; //고정소수점 값 원본을 반환
+	void setRawBits(int const raw); //고정소수점 값 원본을 설정
 };
 
 #endif
