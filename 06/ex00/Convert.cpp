99c5c34695a9bbff137b055eaf4f32d9120195f4
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-12-03 23:15:06 +0900

    ex00

--- 06/ex00/Convert.cpp
+++ 06/ex00/Convert.cpp
@@ -6,64 +6,140 @@
 /*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 13:55:20 by hyko              #+#    #+#             */
-/*   Updated: 2022/12/02 19:33:24 by hyko             ###   ########.fr       */
+/*   Updated: 2022/12/03 22:24:12 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Convert.hpp"
 
-Convert::Convert(char* data) : _data(data)
+Convert::Convert(char* data) : _data(data) {}
+
+Convert::~Convert() {}
+
+void    Convert::_infnan(void)
 {
+    if (!strcmp(_data, "-inff") || !strcmp(_data, "-inf"))
+        _type = "n_inf";
+    if (!strcmp(_data, "+inff") || !strcmp(_data, "+inf"))
+        _type = "p_inf";
+    if (!strcmp(_data, "nanf") || !strcmp(_data, "nan"))
+        _type = "nan";
 }
 
-Convert::~Convert()
-{
-    std::cout << "type: " << _type << std::endl;
-}
-
-void    Convert::checkDataType(void)
+void    Convert::_checkDataType(void)
 {
     int len = strlen(_data);
+    bool p_flag = false;
     
-    //char -> 1글자, 숫자X
     if (len == 1 && isdigit(_data[0]) == false) {
         _type = "char";
         return ;
     }
-
-    //int -> 숫자로만 이루어짐
-    int i = 0;
-    if (_data[0] == '-' || _data[0] == '+')
-        i++;
-    while (_data[i]) {
-        if (isdigit(_data[i]) == false)
-            break ;
-        i++;
-    }
-    if (i == len) {
-        _type = "int";
+    if (_data[0] != '+' && _data[0] != '-' && !isdigit(_data[0])) {
         return ;
     }
-
-    int p = 0;
-    int f = 0;
-    
-    //float -> 숫자 중간에 '.' 있고, 'f'로 끝남
-    if (_data[len - 1] == 'f') {
-        _type = "float";
-        return ;
+    for (int i = 1; i < len; i++) {
+        if (!isdigit(_data[i])) { 
+            if (p_flag == false && _data[i] == '.' && _data[i + 1])
+                p_flag = true;
+            else if (_data[i] == 'f' && !_data[i + 1]) {
+                _type = "float";
+                return ;
+            }
+            else
+                return ;
+        }
     }
-
-    //double -> 숫자 중간에 '.'있음
-    else {
+    if (p_flag == true)
         _type = "double";
+    else
+        _type = "int";
+}
+
+void    Convert::execute(void)
+{   
+    _checkDataType();
+    _infnan();
+
+    if (_type == "char")
+        _printChar(_data[0]);
+    else if (_type == "int")
+        _printInt(atoi(_data));
+    else if (_type == "float")
+        _printFloat(static_cast<float>(atof(_data)));
+    else if (_type == "double")
+        _printDouble(atof(_data));
+    else if (_type == "n_inf")
+        // std::cout << "nan" << std::endl;
+    else if (_type == "p_inf")
+        // std::cout << "nan" << std::endl;
+    else if (_type == "nan")
+        // std::cout << "nan" << std::endl;
+    else
+        throw (InvalidArgumentException());
+}
+
+void    Convert::_printChar(char c)
+{
+    std::cout << "char: " << c << std::endl;
+    std::cout << "int: " << static_cast<int>(c) << std::endl;
+    std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+    std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
+}
+
+void    Convert::_printInt(int i)
+{
+    if (isprint(i))
+        std::cout << "char: " << static_cast<char>(i) << std::endl;
+    else
+        std::cout << "char: Non displayable" << std::endl;
+    std::cout << "int: " << i << std::endl;
+    std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
+    std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
+}
+
+void    Convert::_printFloat(float f)
+{   
+    char c = static_cast<char>(f);
+    if (isprint(c))
+        std::cout << "char: " << c << std::endl;
+    else
+        std::cout << "char: Non displayable" << std::endl;
+    std::cout << "int: " << static_cast<int>(f) << std::endl;
+    if (f == static_cast<int>(f)) {
+        std::cout << "float: " << f << ".0f" << std::endl;
+        std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
+    }
+    else {
+        std::cout << "float: " << f << "f" << std::endl;
+        std::cout << "double: " << static_cast<double>(f) << std::endl;
     }
 }
 
+void    Convert::_printDouble(double d)
+{    
+    char c = static_cast<char>(d);
+    if (isprint(c))
+        std::cout << "char: " << c << std::endl;
+    else
+        std::cout << "char: Non displayable" << std::endl;
+    std::cout << "int: " << static_cast<int>(d) << std::endl;
+    if (d == (int)d) {
+        std::cout << "float: " << static_cast<float>(d) << ".0f" << std::endl;
+        std::cout << "double: " << d << ".0" << std::endl;
+    }
+    else {
+        std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+        std::cout << "double: " << d << std::endl;
+    }
+}
 
-const char* Convert::TooManyArgumentException::what() const throw()
+void    Convert::_printInfNan(void)
 {
-	return ("[Exception] Too Many Argument");
+    std::cout << "char: " << c << std::endl;
+    std::cout << "int: " << static_cast<int>(c) << std::endl;
+    std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+    std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
 }
 
 const char* Convert::InvalidArgumentException::what() const throw()
--- 06/ex00/Convert.hpp
+++ 06/ex00/Convert.hpp
@@ -6,7 +6,7 @@
 /*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 13:55:18 by hyko              #+#    #+#             */
-/*   Updated: 2022/12/02 19:25:32 by hyko             ###   ########.fr       */
+/*   Updated: 2022/12/03 22:05:10 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 # define CONVERT_HPP
 
 #define DEFAULT "\033[0m"
-#define BLACK "\033[0;30m"
 #define RED "\033[0;31m"
-#define GREEN "\033[0;32m"
-#define YELLOW "\033[0;33m"
-#define BLUE "\033[0;34m"
-#define PURPLE "\033[0;35m"
-#define CYAN "\033[0;36m"
-#define WHITE "\033[0;37m"
 
 #include <iostream>
 #include <cstring>
@@ -31,20 +24,26 @@ class Convert
 private:
     char*       _data;
     std::string _type;
+    void        _checkDataType(void);
+    void        _infnan(void);
+    void        _printChar(char c);
+    void        _printInt(int i);
+    void        _printFloat(float f);
+    void        _printDouble(double d);
+
+    Convert(void);
+    Convert(const Convert& obj);
+    Convert&    operator=(const Convert& obj);
+
 public:
     Convert(char* data);
     ~Convert(void);
 
-    void    checkDataType(void);
+    void    execute(void);
 
-    class   TooManyArgumentException : public std::exception {
-        const char* what() const throw();
-    };
     class   InvalidArgumentException : public std::exception {
         const char* what() const throw();
     };
 };
-
-
 
 #endif
