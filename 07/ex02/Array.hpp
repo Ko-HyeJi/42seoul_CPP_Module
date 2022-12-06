d41ee95ead06a88c4792f373f670a5d529b803a0
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-12-07 04:23:24 +0900

    ex02

---
+++ 07/ex02/Array.hpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 23:36:33 by hyko              #+#    #+#             */
+/*   Updated: 2022/12/07 04:21:20 by hyko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+#include <exception>
+
+template <typename T>
+class Array
+{
+private:
+	T*				_arr;
+	unsigned int	_size;
+
+public:
+	Array(void) : _arr(NULL), _size(0) {}
+	Array(unsigned int n) : _arr(NULL), _size(n) {
+		if (_size)
+			_arr = new T[_size];
+	}
+	Array(const Array& obj) {
+		*this = obj;
+	}
+	~Array(void) {
+		if (_size) {
+			delete[] _arr;
+			_arr = NULL;
+			_size = 0;
+		}
+	}
+	
+	Array& operator=(const Array& obj) {
+		if (this != &obj) {
+			if (_size) {
+				delete[] _arr;
+				_arr = NULL;
+				_size = 0;
+			}
+			_size = obj.size();
+			if (_size) 
+				_arr = new T[_size];
+			for (unsigned int i = 0; i < _size; i++) {
+				_arr[i] = obj[i];
+			}
+		}
+		return (*this);
+	}
+
+	T&	operator[](const unsigned int idx) {
+		// std::cout << "operator [] called" << std::endl;
+		if (idx >= _size)
+			throw (InvalidIndexException());
+		return _arr[idx];
+	}
+	T& operator[](const unsigned int idx) const {
+		// std::cout << "const operator [] called" << std::endl;
+		if (idx >= _size)
+			throw (InvalidIndexException());
+		return _arr[idx];
+	}
+
+	unsigned int	size(void) const {
+		return (_size);
+	}
+
+	class InvalidIndexException : public std::exception {
+	public:
+		const char* what(void) const throw() {
+			return ("Exception: Invalid Index");
+		}
+	};
+	
+	void	printArray(void) {
+		for (unsigned int i = 0; i < _size; i++)
+			std::cout << _arr[i] << std::endl;
+	}
+};
+
+#endif
---
+++ 07/ex02/main.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 23:36:35 by hyko              #+#    #+#             */
+/*   Updated: 2022/12/07 04:04:11 by hyko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+// int	main(void)
+// {
+// 	try
+// 	{
+// 		Array<char> arr(10);
+// 		for (int i = 0; i < 10; i++) {
+// 			arr[i] = 'a' + i;
+// 		}
+// 		arr.printArray();
+		
+// 		std::cout << std::endl;
+		
+// 		Array<char> copy(5);
+// 		copy = arr;
+// 		copy.printArray();
+
+// 		std::cout << std::endl;
+		
+// 		std::cout << arr[-1] << std::endl;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << std::endl;
+// 	}
+// }
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+    
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
