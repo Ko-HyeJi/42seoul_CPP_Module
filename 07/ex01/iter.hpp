2263d1e7180aae49bc6dedf93a46277cb752532e
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-12-07 05:00:26 +0900

    ex01

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -3,22 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:46:30 by hyko              #+#    #+#             */
-/*   Updated: 2022/12/05 16:05:05 by hyko             ###   ########.fr       */
+/*   Updated: 2022/12/07 04:58:44 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+
 template <typename T>
-void iter(T* arr, int length, void (*fn)(T&))
+void iter(T* arr, int length, void (*f)(T&))
 {
-	for (int i = 0; i < length, i++) {
+	for (int i = 0; i < length; i++) {
 		f(arr[i]);
 	}
+	std::cout << std::endl;
+}
+
+template <typename T>
+void	printFunc(T& arr)
+{
+	std::cout << arr << " ";
 }
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -3,10 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:46:38 by hyko              #+#    #+#             */
-/*   Updated: 2022/12/05 15:16:27 by hyko             ###   ########.fr       */
+/*   Updated: 2022/12/07 04:57:45 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+int main(void)
+{
+    int			intArr[5] = {1, 2, 3, 4, 5};
+    float		floatArr[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    char		charArr[5] = {'a', 'b', 'c', 'd', 'e'};
+    std::string	stringArr[3] = {"hello", "42", "seoul"};
+
+    iter(intArr, sizeof(intArr) / sizeof(int), printFunc);
+    iter(floatArr, sizeof(floatArr) / sizeof(float), printFunc);
+    iter(charArr, sizeof(charArr) / sizeof(char), printFunc);
+    iter(stringArr, sizeof(stringArr) / sizeof(std::string), printFunc);
+}
