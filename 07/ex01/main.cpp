53fc3fc8bee80c95e599729091c80450d57b7ed3
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-12-08 12:55:16 +0900

    ex01

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:46:38 by hyko              #+#    #+#             */
-/*   Updated: 2022/12/07 10:21:16 by hyko             ###   ########.fr       */
+/*   Updated: 2022/12/08 12:51:29 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,23 @@ int main(void)
     const int constIntArr[5] = {1, 2, 3, 4, 5};
     
     std::cout << "\ncalling iter with printer" << std::endl;
+    iter(intArr, sizeof(intArr) / sizeof(int), printer);
+    std::cout << "\ncalling iter with printer" << std::endl;
     iter(charArr, sizeof(charArr) / sizeof(char), printer);
     std::cout << "\ncalling iter with printer" << std::endl;
     iter(floatArr, sizeof(floatArr) / sizeof(float), printer);
     std::cout << "\ncalling iter with printer" << std::endl;
     iter(stringArr, sizeof(stringArr) / sizeof(std::string), printer);
-    std::cout << "\ncalling iter with printer" << std::endl;
-    iter(intArr, sizeof(intArr) / sizeof(int), printer);
+    std::cout << std::endl;
+    
     std::cout << "\ncalling iter with addNum" << std::endl;
     iter(intArr, sizeof(intArr) / sizeof(int), addNum);
     std::cout << "calling iter with printer" << std::endl;
     iter(intArr, sizeof(intArr) / sizeof(int), printer);
+    std::cout << std::endl;
+    
     std::cout << "\ncalling iter with printer" << std::endl;
     iter(constIntArr, sizeof(constIntArr) / sizeof(const int), printer);
-    std::cout << std::endl;
     // iter(constIntArr, sizeof(constIntArr), addNum);
+    std::cout << std::endl;
 }
