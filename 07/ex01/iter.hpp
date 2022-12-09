b92fd8b4e5322fe9f9bb8494cdbd693d60eb47b2
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-12-09 14:34:26 +0900

    ex01

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:46:30 by hyko              #+#    #+#             */
-/*   Updated: 2022/12/09 13:39:23 by hyko             ###   ########.fr       */
+/*   Updated: 2022/12/09 14:26:23 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* arr, int length, void (*f)(T&))
+void iter(T* arr, unsigned int length, void (*f)(T&))
 {
-	for (int i = 0; i < length; i++) {
+	for (unsigned int i = 0; i < length; i++) {
 		f(arr[i]);
 	}
 }
 
 template <typename T>
-void iter(const T* arr, const int length, void (*f)(const T&))
+void iter(const T* arr, const unsigned int length, void (*f)(const T&))
 {
-	for (int i = 0; i < length; i++) {
+	for (unsigned int i = 0; i < length; i++) {
 		f(arr[i]);
 	}
 }
